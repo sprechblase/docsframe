2f82ebefd0891e6dbe35f82e2ab286eef8728a1c
Samuel Krebs <hi@skre.dev>
2024-10-13 17:44:07 +0200

    fix: remove use-config file

--- registry/registry-lib.ts
+++ registry/registry-lib.ts
@@ -2,17 +2,6 @@ import { Registry } from "@/registry/schema";
 
 export const lib: Registry = [
   {
-    name: "use-config",
-    type: "registry:lib",
-    dependencies: ["jotai", "jotai/utils"],
-    files: [
-      {
-        path: "lib/use-config.ts",
-        type: "registry:lib",
-      },
-    ],
-  },
-  {
     name: "use-mounted",
     type: "registry:lib",
     dependencies: ["react"],
