cc43c49aef79ec9b5df7a668de801582abc22e7a
Samuel Krebs <hi@skre.dev>
2024-10-26 19:18:14 +0200

    fix: remove mainNav

--- registry/default/types/index.d.ts
+++ registry/default/types/index.d.ts
@@ -2,21 +2,15 @@ export interface NavItem {
   title: string;
   href?: string;
   disabled?: boolean;
-  external?: boolean;
-  icon?: keyof typeof Icons;
   label?: string;
-  event?: string;
 }
 
 export interface NavItemWithChildren extends NavItem {
   items?: NavItemWithChildren[];
 }
 
-export interface MainNavItem extends NavItem {}
-
 export interface SidebarNavItem extends NavItemWithChildren {}
 
 export type DashboardConfig = {
-  mainNav: MainNavItem[];
   sidebarNav: SidebarNavItem[];
 };
