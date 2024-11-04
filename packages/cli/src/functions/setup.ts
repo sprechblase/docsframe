08866032eeabf9c8902b9c87dc75cc16e2563b81
Samuel Krebs <hi@skre.dev>
2024-11-04 20:47:15 +0100

    feat(package): move globalcss editing to setup

--- packages/cli/src/functions/setup.ts
+++ packages/cli/src/functions/setup.ts
@@ -7,7 +7,21 @@ interface SetupProps {
   dir: string;
 }
 
-export async function setup({
+export async function setup(props: SetupProps) {
+  const { dir } = props;
+  try {
+    await Promise.all([
+      createDocsframeJson(props),
+      updateTsconfig(dir),
+      updateNextConfig(dir),
+      appendGlobalStyles(dir),
+    ]);
+  } catch (error) {
+    console.error("An error occurred during the setup process:", error);
+  }
+}
+
+async function createDocsframeJson({
   contributionOwner,
   contributionRepo,
   dir,
@@ -29,32 +43,32 @@ export async function setup({
     },
   };
 
-  await fs.writeFileSync(
-    docsframeJsonPath,
-    JSON.stringify(docsframeJson, null, 2)
-  );
+  await fs.writeFile(docsframeJsonPath, JSON.stringify(docsframeJson, null, 2));
+}
 
+async function updateTsconfig(dir: string) {
   const tsconfigJsonPath = path.join(dir, "tsconfig.json");
   const tsconfigJson = await fs.readJSON(tsconfigJsonPath);
-  const tsconfigContentCollections = {
-    "content-collections": ["./.content-collections/generated"],
-  };
 
   tsconfigJson.compilerOptions = tsconfigJson.compilerOptions || {};
   tsconfigJson.compilerOptions.paths = tsconfigJson.compilerOptions.paths || {};
 
-  Object.assign(tsconfigJson.compilerOptions.paths, tsconfigContentCollections);
+  tsconfigJson.compilerOptions.paths["content-collections"] = [
+    "./.content-collections/generated",
+  ];
 
   await fs.writeJSON(tsconfigJsonPath, tsconfigJson, { spaces: 2 });
+}
 
+async function updateNextConfig(dir: string) {
   const nextConfigPath = path.join(dir, "next.config.mjs");
   let nextConfigContent = await fs.readFile(nextConfigPath, "utf8");
 
   if (!nextConfigContent.includes("withContentCollections")) {
     nextConfigContent = `
-  import { withContentCollections } from "@content-collections/next";
-  ${nextConfigContent}
-      `.trim();
+import { withContentCollections } from "@content-collections/next";
+${nextConfigContent}
+    `.trim();
   }
 
   if (!nextConfigContent.includes("export default withContentCollections(")) {
@@ -64,5 +78,30 @@ export async function setup({
     );
   }
 
-  await fs.writeFileSync(nextConfigPath, nextConfigContent);
+  await fs.writeFile(nextConfigPath, nextConfigContent);
+}
+
+async function appendGlobalStyles(dir: string) {
+  const globalsCssPath = path.join(dir, "app", "globals.css");
+  const additionalCss = `
+
+@layer utilities {
+  .step {
+    counter-increment: step;
+  }
+
+  .step:before {
+    @apply absolute inline-flex h-9 w-9 items-center justify-center rounded-full border-4 border-background bg-muted text-center -indent-px font-mono text-base font-medium;
+    @apply ml-[-50px] mt-[-4px];
+    content: counter(step);
+  }
+}
+
+@media (max-width: 640px) {
+  .container {
+    @apply px-4;
+  }
+}`;
+
+  await fs.appendFile(globalsCssPath, additionalCss, "utf8");
 }
