219cd473fb166be6d1ab2dc8bb6da66c4204d1f0
Samuel Krebs <hi@skre.dev>
2024-11-04 18:16:49 +0100

    fix: remove unused code

--- registry/index.ts
+++
@@ -1,7 +0,0 @@
-import { examples } from "./registry-examples";
-import { ui } from "./registry-ui";
-import { lib } from "./registry-lib";
-import { types } from "./registry-types";
-import { Registry } from "./schema";
-
-export const registry: Registry = [...ui, ...lib, ...types, ...examples];
