fd1559bf05331b336a6e9876dd11d301cad0373f
Samuel Krebs <hi@skre.dev>
2024-11-05 15:28:37 +0100

    feat(package): move init to commands

---
+++ packages/cli/src/commands/init.ts
@@ -0,0 +1,91 @@
+#!/usr/bin/env node
+console.clear();
+
+import type { PackageManager } from "../types";
+
+import {
+  intro,
+  outro,
+  confirm,
+  select,
+  spinner,
+  text,
+  log,
+} from "@clack/prompts";
+import { setTimeout as sleep } from "node:timers/promises";
+import color from "picocolors";
+
+import path from "node:path";
+import { installDeps } from "../functions/installDeps";
+import { copyTemplates } from "../functions/copyTemplate";
+import { setup } from "../functions/setup";
+import { Command } from "commander"
+
+export const init = new Command()
+  .name("init")
+  .description("initialize your project and install dependencies")
+  .action(async () => {
+
+
+intro(color.inverse(" Docsframe "));
+
+const dir = path.resolve(
+  process.cwd(),
+  (await text({
+    message: "Enter a project directory:",
+    placeholder: "Leave blank for current directory",
+    defaultValue: ".",
+  })) as string
+);
+
+const manager = (await select({
+  message: "Select a package manager:",
+  options: [
+    { label: "npm", value: "npm" },
+    { label: "pnpm", value: "pnpm" },
+    { label: "yarn", value: "yarn" },
+  ],
+})) as PackageManager;
+
+const githubRepo = await confirm({
+  message: "Does your project have a GitHub Repo?",
+});
+
+let contributionOwner = "";
+let contributionRepo = "";
+
+if (githubRepo) {
+  contributionOwner = (await text({
+    message: "Enter your repository owners GitHub username:",
+    placeholder: "Leave blank if you are not using a GitHub Repo",
+    defaultValue: "",
+  })) as string;
+
+  contributionRepo = (await text({
+    message: "Enter your GitHub repository name:",
+    placeholder: "Leave blank if you are not using a GitHub Repo",
+    defaultValue: "",
+  })) as string;
+}
+
+const s = spinner();
+s.start("Setting up Docsframe. This may take some time.");
+await sleep(100);
+
+try {
+  await installDeps({ manager, dir, stdio: "inherit" });
+  await copyTemplates({ dir });
+  await setup({ contributionOwner, contributionRepo, dir });
+} catch (error) {
+  log.error("Error while setting up Docsframe.");
+  log.info(error as string);
+  process.exit(1);
+} finally {
+  s.stop("Installed via " + manager);
+}
+
+outro("You're all set, thank you for choosing Docsframe!");
+
+await sleep(1000);
+
+})
--- packages/cli/src/index.ts
+++ packages/cli/src/index.ts
@@ -1,82 +1,25 @@
 #!/usr/bin/env node
-console.clear();
+import { init } from "./commands/init"
+import { Command } from "commander"
 
-import type { PackageManager } from "./types";
+import packageJson from "../package.json"
 
-import {
-  intro,
-  outro,
-  confirm,
-  select,
-  spinner,
-  text,
-  log,
-} from "@clack/prompts";
-import { setTimeout as sleep } from "node:timers/promises";
-import color from "picocolors";
+process.on("SIGINT", () => process.exit(0))
+process.on("SIGTERM", () => process.exit(0))
 
-import path from "node:path";
-import { installDeps } from "./functions/installDeps";
-import { copyTemplates } from "./functions/copyTemplate";
-import { setup } from "./functions/setup";
+async function main() {
+  const program = new Command()
+    .name("docsframe")
+    .description("add a simple documentation framework")
+    .version(
+      packageJson.version || "1.0.0",
+      "-v, --version",
+      "display the version number"
+    )
 
-intro(color.inverse(" Docsframe "));
+  program.addCommand(init)
 
-const dir = path.resolve(
-  process.cwd(),
-  (await text({
-    message: "Enter a project directory:",
-    placeholder: "Leave blank for current directory",
-    defaultValue: ".",
-  })) as string
-);
-
-const manager = (await select({
-  message: "Select a package manager:",
-  options: [
-    { label: "npm", value: "npm" },
-    { label: "pnpm", value: "pnpm" },
-    { label: "yarn", value: "yarn" },
-  ],
-})) as PackageManager;
-
-const githubRepo = await confirm({
-  message: "Does your project have a GitHub Repo?",
-});
-
-let contributionOwner = "";
-let contributionRepo = "";
-
-if (githubRepo) {
-  contributionOwner = (await text({
-    message: "Enter your repository owners GitHub username:",
-    placeholder: "Leave blank if you are not using a GitHub Repo",
-    defaultValue: "",
-  })) as string;
-
-  contributionRepo = (await text({
-    message: "Enter your GitHub repository name:",
-    placeholder: "Leave blank if you are not using a GitHub Repo",
-    defaultValue: "",
-  })) as string;
+  program.parse()
 }
 
-const s = spinner();
-s.start("Setting up Docsframe. This may take some time.");
-await sleep(100);
-
-try {
-  await installDeps({ manager, dir, stdio: "inherit" });
-  await copyTemplates({ dir });
-  await setup({ contributionOwner, contributionRepo, dir });
-} catch (error) {
-  log.error("Error while setting up Docsframe.");
-  log.info(error as string);
-  process.exit(1);
-} finally {
-  s.stop("Installed via " + manager);
-}
-
-outro("You're all set, thank you for choosing Docsframe!");
-
-await sleep(1000);
+main()
